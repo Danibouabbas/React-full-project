9c6d1cd3425ec2dfc742b2e2bfe33cd248a0592c
Danibouabbas <12232606@students.liu.edu.lb>
2026-01-01 21:52:04 +0200

    add im htttt

--- finalweb2React_Project/reactproject/backend/server.js
+++ finalweb2React_Project/reactproject/backend/server.js
@@ -25,12 +25,15 @@ if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath);
 app.use("/uploads", express.static(uploadPath));
 
 /* ================= Database Connection ================= */
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 db.connect((err) => {
