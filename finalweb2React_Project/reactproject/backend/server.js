c5aab6d51a4547d4baf805672d3e6f122d639b70
Danibouabbas <12232606@students.liu.edu.lb>
2026-01-01 21:10:34 +0200

    Remove im ht

--- finalweb2React_Project/reactproject/backend/server.js
+++ finalweb2React_Project/reactproject/backend/server.js
@@ -1,4 +1,4 @@
-
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
@@ -22,15 +22,12 @@ if (!fs.existsSync(uploadPath)) {
 app.use("/uploads", express.static(uploadPath));
 
 /* ================= Database Connection ================= */
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.getConnection((err, connection) => {
